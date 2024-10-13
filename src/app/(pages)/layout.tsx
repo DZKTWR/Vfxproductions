6ef7f036a57b9563141cb024e02bf30ec40b882b
DZKTWR <diegogemer45@gmail.com>
2024-10-12 20:26:36 -0500

    main

--- src/app/(pages)/layout.tsx
+++ src/app/(pages)/layout.tsx
@@ -3,7 +3,7 @@
 import Navbar2 from "@/components/Navbar2";
 import { AnimatePresence } from 'framer-motion';
 import { usePathname } from 'next/navigation';
-import Curve from '@/components/curve/curve';
+import Curve from "@/components/curveanim/curve";
 
 export default function Layout (
     { children }: { children: React.ReactNode }) 
