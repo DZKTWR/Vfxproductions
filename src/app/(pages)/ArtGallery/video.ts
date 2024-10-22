5fc64bef1c92c46c7245c30e8ae28955cf2600c3
DZKTWR <diegogemer45@gmail.com>
2024-10-21 20:00:55 -0500

    Update video.ts

--- src/app/(pages)/ArtGallery/video.ts
+++ src/app/(pages)/ArtGallery/video.ts
@@ -8,13 +8,13 @@ interface Video {
   const videos: Video [] = [
     {
       id: "1-0",
-      videoId: '<iframe width="560" height="315" src="https://www.youtube.com/embed/LiJmxnwm4kY?si=5BiELNyGqBiEYyfJ" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>',
+      videoId: 'https://www.youtube.com/embed/LiJmxnwm4kY?si=5BiELNyGqBiEYyfJ',
       videoName: "Kaoruko Waguri Edit 4K - DAZKON ",
       previewImg: '/Gallery/1.png',
     },
     {
       id: "1-2",
-      videoId: '<iframe width="468" height="832" src="https://www.youtube.com/embed/fX_nQwgKRjk" title="" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>',
+      videoId: 'https://www.youtube.com/embed/fX_nQwgKRjk,
       videoName: "Chisato | Edit - DAZKON ",
       previewImg: '/Gallery/chisato.png',
     },
@@ -26,31 +26,31 @@ interface Video {
     },
     {
       id: "2-0",
-      videoId: '<iframe width="468" height="832" src="https://www.youtube.com/embed/4RFz06VlIj0" title="Hakari ♥" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>',
+      videoId: 'https://www.youtube.com/embed/4RFz06VlIj0',
       videoName: "Hakari | Edit - DAZKON ",
       previewImg:  '/Gallery/Mintk_1_32.1.3.png',
     },
     {
       id: "2-2",
-      videoId: '<iframe width="560" height="315" src="https://www.youtube.com/embed/3X6Qi3ineIc?si=P56Fgj3A2bEOkULv" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>',
+      videoId: 'https://www.youtube.com/embed/3X6Qi3ineIc?si=P56Fgj3A2bEOkULv',
       videoName: "Miku Nakano - DAZKON ",
       previewImg:  '/Gallery/MinYt.png',
     },
     {
       id: "2-4",
-      videoId: '<iframe width="468" height="832" src="https://www.youtube.com/embed/KLEVApBzsKU" title="Masha 4K Edit | Alya Sometimes Hides Her Feelings in Russian" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>',
+      videoId: 'https://www.youtube.com/embed/KLEVApBzsKU',
       videoName: "Masha 4K Edit | Dazkon ",
       previewImg: '/Gallery/StsAlya_27.1.1.png',
     },
     {
       id: "3-0",
-      videoId: '<iframe width="468" height="832" src="https://www.youtube.com/embed/NWNbPHXz0-w" title="alya #anime #animeedit #amv #alya #waifuedit #waifu" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>',
+      videoId: 'https://www.youtube.com/embed/NWNbPHXz0-w',
       videoName: "Alya | Edit - DAZKON ",
       previewImg:  '/Gallery/Untitled_29.1.2.png',
     },
     {
       id: "3-2",
-      videoId: '<iframe width="468" height="832" src="https://www.youtube.com/embed/6Q8D1y5j7_M" title="Villhaze Edit Remake(The Vexations of a Shut-In Vampire Princess)" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>',
+      videoId: 'https://www.youtube.com/embed/6Q8D1y5j7_M',
       videoName: "Villhaze Edit Remake - dazkon",
       previewImg: '/Gallery/Vt.png',
     },
